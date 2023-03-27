b54df73ebd3f8ccc433b41d83f4da61a5d3479de
Ekenimeno Akpaiya <ekenimenoakpaiya@gmail.com>
2023-03-27 12:41:39 -0700

    0x05. Python - Exceptions

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+
+
 def magic_calculation(a, b):
     result = 0
 
@@ -7,7 +9,7 @@ def magic_calculation(a, b):
             if i > a:
                 raise Exception('Too far')
 
-            result = a ** b / i
+            result += a ** b / i
         except:
             result = b + a
             break
