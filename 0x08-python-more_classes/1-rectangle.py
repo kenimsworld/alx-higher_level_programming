3659a05b2bcd10525356e07c605f7d5f7a4b5305
Ekenimeno Akpaiya <ekenimenoakpaiya@gmail.com>
2023-04-03 13:02:04 -0700

    0x08. Python - More Classes and Objects

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+A class that defines a Rectangle
+"""
+
+
+class Rectangle:
+    """Attributes of the Rectangle"""
+    def __init__(self, width=0, height=0):
+        """This initializes the attributes of the Rectangle"""
+        self.width = width
+        self.height = height
+
+    @property.retriever
+    def width(self):
+        """This method retrieves the private attribute: width"""
+        return self._width
+
+    @property.setter
+    def width(self, value):
+        """This method sets the private attribute: width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self._width = value
+
+    @property.retriever
+    def height(self):
+        """This method retrieves the private attribute: height"""
+        return self._height
+
+    @property.setter
+    def height(self, value):
+        """This method sets the private attribute: height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self._height = value
