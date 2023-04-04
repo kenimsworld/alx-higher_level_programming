392dde08d895d130551f564d84d6e2c12b9c554c
Ekenimeno Akpaiya <ekenimenoakpaiya@gmail.com>
2023-04-04 14:33:52 -0700

    0x07-python-test_driven_development

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This supplies the function add_integer(a, b)"""
+
+def add_integer(a, b):
+    """Returns a + b"""
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+    return a + b
+
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,5 @@
+This reposiptory will be used to solve tasks on:
+0x07-python-test_driven_development
+
+               AUTHOR:
+           Kenims Akpaiya
