83443571ee22e65c0874359efba6d2b994714573
Ekenimeno Akpaiya <ekenimenoakpaiya@gmail.com>
2023-04-04 02:16:49 -0700

    0x08-python-more_classes

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -60,4 +60,4 @@ class Rectangle:
 
     def __repr__(self):
         """This returns a string representation of the rectangle for reproduction"""
-        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
+        return ("Rectangle({:d}, {:d})".format(self.__width(), self.__height()))
