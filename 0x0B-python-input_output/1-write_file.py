ab95a5b66bd42e769b709834f80150f7b87b3d32
Ekenimeno Akpaiya <ekenimenoakpaiya@gmail.com>
2023-04-12 14:31:08 -0700

    0x0B-python-input_output

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+"""
+Contains the function "wrtie_file"
+"""
+
+
 def write_file(filename="", text=""):
     """function that writes a string to a text file and return the number
     of characters written"""
-
     with open(filename, 'w',encoding='utf=8') as fil:
         return fil.write(text)
