2741fa3fe1541da826784f9ffd148e90bb4ee910
Ekenimeno Akpaiya <ekenimenoakpaiya@gmail.com>
2023-04-12 14:18:19 -0700

    0x0B-python-input_output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,7 +3,8 @@
 contians the read_file function
 """
 
+
 def read_file(filename=""):
     """reads a UTF8 text file and print it to stdout"""
-    with open(filename, "r", encoding="utf-8") as file:
-        print(file.read(), end="")
+    with open(filename, "r", encoding="utf-8") as fil:
+        print(fil.read(), end="")
