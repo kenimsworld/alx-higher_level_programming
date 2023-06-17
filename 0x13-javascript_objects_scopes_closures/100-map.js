9158daae1dda940c231b28c3ca271037ae4cf7ed
Ekenimeno Akpaiya <ekenimenoakpaiya@gmail.com>
2023-06-17 13:40:48 -0700

    0x13-javascript_objects_scopes_closures

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
+const newList = list.map(function (num, index) {
+  return num * index;
+});
+
 console.log(list);
-const newList = list.map((value, index) => value * index);
-console.log(newList);#!/usr/bin/node
-const list = require('./100-data').list;
-console.log(list);
-const newList = list.map((value, index) => value * index);
 console.log(newList);
