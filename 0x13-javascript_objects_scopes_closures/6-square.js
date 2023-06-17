95edd0aac8d5f5738c5957d6b65fd55f1f9a105d
Ekenimeno Akpaiya <ekenimenoakpaiya@gmail.com>
2023-06-17 03:28:30 -0700

    0x13-javascript_objects_scopes_closures

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,16 +1,14 @@
 #!/usr/bin/node
 const Square01 = require('./5-square');
 
-class Square extends Square01 {
+module.exports = class Square extends Square01 {
   charPrint (c) {
     if (c === undefined) {
       this.print();
     } else {
-      for (let i = 0; i < this.height; i++) {
+      for (let m = 0; m < this.height; m++) {
         console.log('C'.repeat(this.width));
       }
     }
   }
-}
-
-module.exports = Square;
+};
