9958793c5ff419928b823ffbc4fc8bc062561aff
Ekenimeno Akpaiya <ekenimenoakpaiya@gmail.com>
2023-07-02 11:45:22 -0700

    0x11-python-network_1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """
-Write a Python script that fetches https://intranet.hbtn.io/status
+Write a Python script that fetches https://alx-intranet.hbtn.io/status
 """
 import urllib.request
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         html = response.read()
     print('Body response:')
     print('\t- type: {}'.format(type(html)))
